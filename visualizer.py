f8b3aae70aa136c458fa21242a04e7abfd635260
Ubuntu <ubuntu@ip-172-31-38-44.eu-north-1.compute.internal>
2024-11-11 19:44:33 +0200

    Weekends highlighted

--- visualizer.py
+++ visualizer.py
@@ -28,13 +28,19 @@ for interval, query in intervals.items():
     # Step 2: Plotting the Data
     plt.figure(figsize=(20, 10))
     for area in pivot_df.columns:
-        plt.plot(pivot_df.index, pivot_df[area], marker='o', label=area)
+        plt.plot(pivot_df.index, pivot_df[area], marker='o', markersize=6, label=area)
 
-    plt.xlabel('Time')
-    plt.ylabel('Queue Length')
-    plt.title(f'Queue Length Over Time by Waiting Area - {interval}')
-    plt.xticks(rotation=45)
-    plt.legend()
+    for i, date in enumerate(pivot_df.index):
+        if date.weekday() in [5, 6]:
+            plt.axvspan(pivot_df.index[i], pivot_df.index[i], color='#FFC9CD', alpha=0.5)
+
+
+    plt.xlabel('Time', fontsize=30)
+    plt.ylabel('Queue Length', fontsize=30)
+    plt.title(f'Queue Length Over Time by Buffer Zone - {interval}', fontsize=30)
+    plt.xticks(rotation=45, fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.legend(fontsize=30, markerscale=1.5)
     plt.grid()
     plt.tight_layout()
     plt.savefig(f'{os.getenv("WORKING_DIR_PATH")}/www/queue_length_visual_{interval.replace(" ", "_")}.png')
